3efb9d76593a7f5b887c065d1530af75e750509b
Wenbin Zhang <wenbinzhang0802@gmail.com>
2016-03-22 23:07:31 -0700

    Setup project and draw checker board

---
+++ CheckerLite/BoardView.swift
@@ -0,0 +1,58 @@
+//
+//  BoardView.swift
+//  CheckerLite
+//
+//  Created by Wenbin Zhang on 3/22/16.
+//  Copyright © 2016 Wenbin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+    var numOfRows: Int
+    var numOfColumns: Int
+    
+    init(frame: CGRect, numOfRows: Int, numOfColumns: Int) {
+        self.numOfRows = numOfRows
+        self.numOfColumns = numOfColumns
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func calculateGridSize() -> CGFloat {
+        return  frame.width / 8.0
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let gridSize = calculateGridSize()
+        let context = UIGraphicsGetCurrentContext()
+        for row in 0..<numOfRows {
+            for column in 0..<numOfColumns {
+                var color: UIColor
+                if row % 2 == 0 {
+                    if column % 2 == 0 {
+                        color = UIColor.blackColor()
+                    } else {
+                        color = UIColor.whiteColor()
+                    }
+                } else {
+                    if column % 2 != 0 {
+                        color = UIColor.blackColor()
+                    } else {
+                        color = UIColor.whiteColor()
+                    }
+                }
+                CGContextSaveGState(context)
+                let rect = CGRect(x: CGFloat(column)*gridSize, y: CGFloat(row)*gridSize, width: gridSize, height: gridSize)
+                CGContextAddRect(context, rect)
+                CGContextSetFillColorWithColor(context, color.CGColor)
+                CGContextFillRect(context, rect)
+                CGContextRestoreGState(context)
+            }
+        }
+    }
+
+}
--- CheckerLite/ViewController.swift
+++ CheckerLite/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let boardView = BoardView(frame: CGRectZero, numOfRows: 8, numOfColumns: 8)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.darkGrayColor()
+        view.addSubview(boardView)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        configureBoardView(view.bounds.size)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        configureBoardView(size)
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+    }
 
+    // MARK: Private stuff
+    private func configureBoardView(size: CGSize) {
+        var boardSize = min(size.width, size.height)
+        var boardOrigin = CGPointZero
+        if size.width > size.height {
+            boardSize = size.height
+            boardOrigin.x = (size.width - size.height) / 2
+        } else {
+            boardSize = size.width
+            boardOrigin.y = (size.height - size.width) / 2
+        }
+        boardView.frame = CGRect(x: boardOrigin.x, y: boardOrigin.y, width: boardSize, height: boardSize)
+    }
 }
 
